95af94c6fbc091ad2b5ea41ecec6629838511aab
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-01-17 17:31:58 +0200

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 
 
@@ -23,6 +25,7 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/projects' element={<Projects/>}/>
       </Routes>
+   <Footer/>
     </BrowserRouter>
   )
 }
