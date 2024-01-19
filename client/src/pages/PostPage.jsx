658fcf7dbf3a4b25ab4f460e5ca33600d484d784
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-01-19 17:07:00 +0200

    add call to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import {Button, Spinner} from 'flowbite-react'
+import CallToAction from "../components/CallToAction";
 export const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -47,6 +48,11 @@ export const PostPage = () => {
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
             <span className="italic">{post &&(post.content.length/1000).toFixed(0)} mins read</span>
         </div>
-        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html:post && post.content}}></div>
+        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html:post && post.content}}>
+
+        </div>
+        <div className="max-w-4xl mx-auto w-full">
+          <CallToAction/>
+        </div>
   </main>;
 };
