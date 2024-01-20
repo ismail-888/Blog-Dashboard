7df125c9c603f373102efccb9e1aeb659bf81fa0
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-01-20 15:43:43 +0200

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,12 @@
 import { Sidebar } from "flowbite-react";
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
+import {
+  HiAnnotation,
+  HiArrowSmRight,
+  HiChartPie,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiUser,
+} from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -9,7 +16,7 @@ import { useSelector } from "react-redux";
 const DashSidebar = () => {
   const location = useLocation();
   const dispatch = useDispatch();
-  const currentUser=useSelector((state)=>state.user.currentUser)
+  const currentUser = useSelector((state) => state.user.currentUser);
   const [tab, setTab] = useState("");
 
   useEffect(() => {
@@ -39,18 +46,25 @@ const DashSidebar = () => {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
+          {currentUser && currentUser.isAdmin && (
+            <Link to={"/dashboard?tab=dash"}>
+              <Sidebar.Item active={tab === "dash" || !tab} icon={HiChartPie} as="div">
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to={"/dashboard?tab=profile"}>
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={currentUser.isAdmin?'Admin':"User"}
+              label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
-          
+
           {currentUser.isAdmin && (
             <Link to={"/dashboard?tab=posts"}>
               <Sidebar.Item
